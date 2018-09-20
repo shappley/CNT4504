06ff28c91672c4a87281d5b9ba63cc49786990ae
Stephen Shappley <n01184718@ospreys.unf.edu>
2018-09-20 07:43:21 -0400

    cleaned up Client code

--- src/main/CNT4504/client/Client.java
+++ src/main/CNT4504/client/Client.java
@@ -4,11 +4,22 @@ import CNT4504.Menu;
 import CNT4504.MenuOption;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 
 public class Client {
+    private Socket socket;
+    private BufferedReader in;
+    private PrintStream out;
+
+    public Client(Socket socket) throws IOException {
+        this.socket = socket;
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.out = new PrintStream(socket.getOutputStream());
+    }
+
     public static void main(String[] args) throws Exception {
         if (args.length < 2) {
             System.out.println("Usage: <address> <port>");
@@ -27,14 +38,22 @@ public class Client {
             }
 
             final Socket socket = new Socket(address, port);
-            final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            final PrintStream out = new PrintStream(socket.getOutputStream());
-
-            out.println(selection);
-            String line;
-            while ((line = in.readLine()) != null) {
-                System.out.println(line);
-            }
+            final Client client = new Client(socket);
+            client.write(selection.toString());
+            System.out.println(client.read());
         }
+    }
+
+    public void write(String data) {
+        out.println(data);
+    }
+
+    public String read() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = in.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        return sb.toString();
     }
 }
