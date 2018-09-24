76346f54c26dccb381e9ed07f9aeb04999d32291
Stephen Shappley <n01184718@ospreys.unf.edu>
2018-09-24 16:17:12 -0400

    Added command option to MultiClientSim

--- src/main/CNT4504/client/MultiClientSim.java
+++ src/main/CNT4504/client/MultiClientSim.java
@@ -6,13 +6,14 @@ import java.io.IOException;
 
 public class MultiClientSim {
     public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("<address><port><number of clients>");
+        if (args.length != 4) {
+            System.out.println("<address><port><option><number of clients>");
             System.exit(1);
         }
 
         final int port = Integer.parseInt(args[1]);
-        final int numberOfClients = Integer.parseInt(args[2]);
+        final int numberOfClients = Integer.parseInt(args[3]);
+        final MenuOption option = MenuOption.valueOf(args[2]);
         final Thread[] clientThreads = new Thread[numberOfClients];
 
         for (int i = 0; i < numberOfClients; i++) {
@@ -20,7 +21,7 @@ public class MultiClientSim {
                 try {
                     long startTime = System.currentTimeMillis();
                     final Client client = new Client(args[0], port);
-                    client.write(MenuOption.NETSTAT.toString());
+                    client.write(option.toString());
                     String response = client.read();
                     long endTime = System.currentTimeMillis();
                     System.out.println((endTime - startTime) + "ms");
